3a962a00561237bb3f0e43de82e6dd31df8860fd
Abhinav-ep <abhinavsureshep2@gmail.com>
2025-04-16 14:02:31 +0530

    Update template3.sh

--- templates/template3.sh
+++ templates/template3.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Simple Math Operations Script
-
-# Define two variables
 num1=<NUM>
 num2=<NUM>
 
-# Calculate sum, difference, and product
 sum=$((num1 + num2))
 diff=$((num1 - num2))
 prod=$((num1 * num2))
 
-# Display the results
 echo "Sum: $sum"
 echo "Difference: $diff"
 echo "Product: $prod"
